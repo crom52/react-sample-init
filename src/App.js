d9f23f1f75f70c5fa273c2320c26edda72370934
th.phuc <tranhoanphuc.vn@smilegate.com>
2023-05-31 14:52:00 +0700

    test

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <div className='App'>
+      <h1> You clicked the button {count} times</h1>
+      <button onClick={incrementCount}>Click Me</button>
       <header className='App-header'>
-        {/* <img
-          src='https://static.cdnlogo.com/logos/r/63/react.svg'
+        <img
+          src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png'
           className='App-logo'
           alt='logo'
-        /> */}
-        <div><a
+        />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
           className='App-link'
           href='https://reactjs.org'
           target='_blank'
@@ -20,11 +25,6 @@ function App() {
         >
           Learn React
         </a>
-          <h1> You clicked the button {count} times</h1>
-          <button onClick={incrementCount}>Click Me</button>
-        </div>
-
-
       </header>
 
     </div>
