78cb860451d7b8398c7c8f1339ce32d3660e9acd
th.phuc <tranhoanphuc.vn@smilegate.com>
2023-05-31 14:39:20 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img
+        {/* <img
           src='https://static.cdnlogo.com/logos/r/63/react.svg'
           className='App-logo'
           alt='logo'
-        />
+        /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
