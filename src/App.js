be76464e7d6b3c8e738c4571fa08ae67d420865b
th.phuc <tranhoanphuc.vn@smilegate.com>
2023-05-31 14:27:35 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='https://static.cdnlogo.com/logos/r/63/react.svg'
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <h1> You clicked the button {count} times</h1>
+        <button onClick={incrementCount}>Click Me</button>
       </header>
+
     </div>
   );
+  function incrementCount() {
+    setCount((previousCount) => previousCount + 1);
+  }
 }
 
 export default App;
