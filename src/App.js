d5e5c931c5b3370ee673f681f47dc5345c57cfbc
th.phuc <tranhoanphuc.vn@smilegate.com>
2023-06-01 09:31:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect, React } from 'react';
 
 function MENU_ID_001() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      // Development mode
+      console.log('Bundle Version:', process.env.REACT_APP_BUNDLE_VERSION);
+      console.log('CSS Version:', process.env.REACT_APP_CSS_VERSION);
+    } else {
+      // Production mode
+      const bundleVersion = window.__BUNDLE_VERSION__;
+      const cssVersion = window.__CSS_VERSION__;
+      console.log('Bundle Version:', bundleVersion);
+      console.log('CSS Version:', cssVersion);
+    }
+  }, []);
+
   return (
     <div className='App'>
       <h1> You clicked the button {count} times</h1>
@@ -20,7 +35,7 @@ function MENU_ID_001() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React ...
+          Learn React
         </a>
       </header>
 
